ea6cd9b44a29a324b6fc559143799f7faf77438e
Mattia Dutto <mattiadutto@Mattias-MacBook-Pro.local>
2022-02-03 16:04:14 +0100

    Remove not useful file

--- constants.py
+++ constants.py
@@ -1,9 +1,7 @@
 # Program constants / server constants
 HOST = "127.0.0.1"
-PORT =  1024 # 0x4A7AB1 could have been a better port, but networkers did not allow us to have it
+PORT = 1024  # 0x4A7AB1 could have been a better port, but networkers did not allow us to have it
 DATASIZE = int(10240 / 4)
 
-# My code:
-NUMBER_OF_CARD = 5 # number of card is the same of # of color that I have on the table
+NUMBER_OF_CARD = 5  # number of card is the same of # of color that I have on the table
 BLUE_TOKEN = 8
-RED_TOKEN = 3 # Not used for now ( 18 / 01 / 2022 )
